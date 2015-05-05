59f1d23b23a4fc9b1a680acd71c97920c2e22c55
JorgeAntonio512 <jorge.antonio.de.segundo@gmail.com>
2015-05-05 17:36:25 -0500

    Added a text field and button. Created random number generator onClick.

--- app/src/main/java/com/example/georgepazdral/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/georgepazdral/funfacts/FunFactsActivity.java
@@ -4,6 +4,11 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class FunFactsActivity extends ActionBarActivity {
@@ -12,6 +17,25 @@ public class FunFactsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //Declare our View variables and assign them the Views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // The button was clicked, so update the fact label with a new fact
+                String fact = "";
+                // Randomly select a fact
+                Random randomGenerator = new Random();   // Construct a new Random number generator
+                int randomNumber = randomGenerator.nextInt(3);
+                fact = randomNumber + "";
+
+                // Update the label with our dynamic fact
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
 
